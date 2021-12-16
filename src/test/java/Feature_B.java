ce713439a330ba42697cc96787a2d99a4dc8b5a0
salim <salimsafak1975@hotmail.com>
2021-12-16 22:27:41 +0100

    Started featureA work5

---
+++ src/test/java/Feature_B.java
@@ -0,0 +1,8 @@
+public class Feature_B {
+
+
+    public static void main(String[] args) {
+        System.out.println("yeni master yaptim newfeature b diye");
+
+    }
+}
--- src/test/java/YENIIIII.java
+++
@@ -1,8 +0,0 @@
-public class YENIIIII {
-
-
-    public static void main(String[] args) {
-        System.out.println(yeni master yaptim newfeature b diye);
-
-    }
-}
