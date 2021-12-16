96be903844df528c0f672fc83b841d3a978f1c98
salim <salimsafak1975@hotmail.com>
2021-12-16 21:35:02 +0100

    added3

--- src/test/java/Hellotest.java
+++ src/test/java/Hellotest.java
@@ -2,6 +2,9 @@ public class Hellotest {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("Localrepo is already publlsihed");
+        System.out.println("Doing more work");
+
 
 
     }
